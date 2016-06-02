91331e990337bd99ceb04cf532cd780232176e43
cesar <cesarg@wolfram.com>
2016-06-02 14:31:47 -0500

    parallel tests

---
+++ starlab/parallel/ini.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+makeplummer -n 100 -i |makemass -f 8 -l 0.1 -u 50 | scale -s > ini.dat
+
+
---
+++ starlab/parallel/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "[`date`] Start"
+cd "./results"
+
+mpirun -np 4 kira -u -D 1 -b 1 -t 1000 -R ini.dat > dyn.dat 2> log.dat
+
+cd ..
+echo "[`date`] End"
+echo "time elapsed: $SECONDS"
+echo all done
