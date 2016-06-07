6705592a8597bc2cc6f5b3035751149f77105b51
cesar <cesarg@wolfram.com>
2016-06-07 09:53:26 -0500

    king with ext field powerlaw

--- starlab/ext-field/king-powerlaw/run1/run.sh
+++ starlab/ext-field/king-powerlaw/run1/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd results
 
-#makeking -n 100 -w 3 -i -u | makemass -f 1 -l 0.5 -u 50 | scale -R 1 -M 1 | add_power_law -G | set_com -r 8000. 0. 0. -v 0 1 0  | kira -u -D 1 -b 1 -t 500 > dyn.dat 2> log.dat
+makeking -n 100 -w 3 -i -u | makemass -f 1 -l 0.5 -u 50 | scale -R 0.1 -M 1 | add_power_law -G | set_com -r 8000. 0. 0. -v 0 3.0 0  | kira -u -D 1 -b 1 -t 100 > dyn.dat 2> log.dat
 
-makeking -n 100 -w 3 -i | makemass -f 1 -l 0.5 -u 50 |scale -R 1 -M 1| add_tidal -F 1 -J 1 | set_com -r 900 900 0 -v 0 1 0 | kira -u -D 1 -b 1 -t 200 > dyn.dat 2> log.dat
 
 cd ..
