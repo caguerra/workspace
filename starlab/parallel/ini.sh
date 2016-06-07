8dfc9a8544fa92fc20e7aae1490c391aced16afd
Cesar Guerra <cesarg@tars.wolfram.com>
2016-06-07 10:57:10 -0400

    parallel testing

--- starlab/parallel/ini.sh
+++ starlab/parallel/ini.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-makeplummer -n 100 -i |makemass -f 8 -l 0.1 -u 50 | scale -s > ini.dat
-
+cd "./results"
+makeplummer -n 400 -i |makemass -f 8 -l 0.1 -u 50 | scale -s > ini.dat
+cd ..
 
--- starlab/parallel/run.sh
+++ starlab/parallel/run.sh
@@ -2,7 +2,9 @@
 echo "[`date`] Start"
 cd "./results"
 
-mpirun -np 4 kira -u -D 1 -b 1 -t 1000 -R ini.dat > dyn.dat 2> log.dat
+(mpirun -np 4 kira -u -D 1 -b 1 -t 1 -R ini.dat) > dyn.dat 2> log.dat 
+#mpirun -np 4 kira -u -D 1 -b 1 -t 1000 -R ini.dat > dyn.dat 2> log.dat
+#kira -u -D 1 -b 1 -t 1000 -R ini.dat > dyn.dat 2> log.dat
 
 cd ..
 echo "[`date`] End"
