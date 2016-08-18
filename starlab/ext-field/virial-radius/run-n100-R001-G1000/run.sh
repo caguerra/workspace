6dcc40c544c2b36dca8a34cd088a994775a3215f
caguerra <caguerra@gmail.com>
2016-08-18 11:55:04 -0500

    virial radius files

---
+++ starlab/ext-field/virial-radius/run-n100-R001-G1000/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir results/
+cd results/
+makeking -n 100 -w 3 -i -u -s 123 | makemass -f 1 -l 0.5 -u 50 | scale -R 0.01 -M 1 -Q 0.5 | add_power_law -G | set_com -r 8000. 0. 0. -v 0 1.0 0 | kira -G 1000 -u -D 1 -b 1 -t 1000 > dyn.dat 2> log.dat
+cd ..
---
+++ starlab/ext-field/virial-radius/run-n100-R1/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir results/
+cd results/
+makeking -n 100 -w 3 -i -u -s 123 | makemass -f 1 -l 0.5 -u 50 | scale -R 1 -M 1 -Q 0.5 | add_power_law -G | set_com -r 8000. 0. 0. -v 0 1.0 0 | kira -u -D 1 -b 1 -t 1000 > dyn.dat 2> log.dat
+cd ..
