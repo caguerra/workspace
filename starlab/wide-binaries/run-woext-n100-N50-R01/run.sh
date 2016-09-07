57b55c5a8a0d835cc8defa11d5735c3ae7e7da48
Cesar Guerra <cguerra@geryon2.(none)>
2016-09-07 16:39:17 -0300

    wide binaries new files

--- starlab/wide-binaries/run-woext-n100-N50-R01/run.sh
+++ starlab/wide-binaries/run-woext-n100-N50-R01/run.sh
@@ -8,10 +8,10 @@ cd "./results"
 echo ""
 echo "running file ${PBS_ARRAYID}"
 cd "./run-${PBS_ARRAYID}"
-#RUN3
-kira -u -D 1 -b 1 -G 20 -t 1000 < ini.dat > dyn.dat 2> log.dat
-#RUN1-2
-#kira -u -D 1 -b 1 -t 1000 < ini.dat > dyn.dat 2> log.dat
+# RUN 2
+kira -u -D 1 -G 20 -b 1 -t 1000 < ini.dat > dyn.dat 2> log.dat
+# RUN 1
+# kira -u -D 1 -b 1 -t 1000 < ini.dat > dyn.dat 2> log.dat
 if [ $? -eq 124 ]; then
 	echo "WARNING: timeout reached for file ${PBS_ARRAYID}"
 fi
