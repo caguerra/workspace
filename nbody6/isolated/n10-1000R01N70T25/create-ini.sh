6483320d5a1cf290da65a38d360a713f257c69cf
cesarg <cesarg@wolfram.com>
2016-12-23 10:47:58 -0500

    modify files

--- nbody6/isolated/n10-1000R01N70T25/create-ini.sh
+++ nbody6/isolated/n10-1000R01N70T25/create-ini.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
-echo "[`date`] Start"
+echo "[`date`] START NBODY SIM"
 
 mkdir ./results
 cd ./results
 
-for numberStars in {500..1000..100}
+stars1=({10..90..10});
+stars2=({100..480..20});
+stars3=({500..1000..50});
+starsAll=("${stars1[@]}" "${stars2[@]}" "${stars3[@]}")
+
+for numberOfStars in ${starsAll[@]}
 do
-	mkdir ./stars-$numberStars
-	cd ./stars-$numberStars
-	num=$(echo "2 * sqrt ($numberStars )" | bc)
-	for randomseed in {100..5000..100}
+	echo "Number of stars: $numberOfStars"
+	mkdir ./stars-$numberOfStars
+	cd ./stars-$numberOfStars
+	if [ "$numberOfStars" -gt "250" ]; 
+	then
+		ETAI=0.02
+		SMAX=1.0
+	else
+		ETAI=0.01
+		SMAX=0.25
+	fi 
+	NNBMAX=$(echo "2 * sqrt ($numberOfStars)" | bc)
+	totalTime=$(echo "37.4175 * sqrt($numberOfStars * 1.000000) / 1"| bc)
+	for fileNumber in {1..70..1}
 	do
-		mkdir "./run-$randomseed/"
-	  	cat << EOF > ./run-$randomseed/ini.dat
+		randomSeed=$RANDOM
+		mkdir "./run-$fileNumber/"
+	  	cat << EOF > ./run-$fileNumber/ini.dat
 1 20.0
-$numberStars 1 5 $randomseed $num 1
-0.02 0.01 0.3 2.0 1.0 500.0 1.0E-03 0.1 0.5
+$numberOfStars 1 5 $randomSeed $NNBMAX 1
+$ETAI 0.01 0.3 2.0 10.0 $totalTime 1.0E-03 0.1 0.5
 0 0 1 0 1 0 0 0 2 0
-0 0 0 0 0 1 0 0 0 0
+0 0 0 0 0 1 0 0 0 6
 0 0 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0 0 1
 0 0 0 0 0 0 0 0 0 0
 2.0E-05 0.001 0.2 1.0 1.0E-06 0.001
-2.3 10.0 0.2 0 0 0.002 0 5.0
-0.5 0 0 0 1.0
+2.3 50.0 0.1 0 0 0.002 0 5.0
+0.5 0 0 0 $SMAX
 EOF
-		echo $randomseed
 	done
 	cd ..
 done
 cd ..
 
 echo `pwd`
-echo "[`date`] End"
-echo "time elapsed: $SECONDS"
-echo all done
+echo "[`date`] END"
+echo "TIME ELAPSED: $SECONDS"
+echo "ALL DONE!"
--- nbody6/isolated/n10-1000R01N70T25/nbody-all.sh
+++ nbody6/isolated/n10-1000R01N70T25/nbody-all.sh
@@ -3,17 +3,31 @@
 echo "[`date`] Start"
 cd ./results
 
-for starfilenumber in {1000..1000..100}
+stars1=({10..90..10});
+stars2=({100..480..20});
+stars3=({500..1000..50});
+starsAll=("${stars1[@]}" "${stars2[@]}" "${stars3[@]}")
+
+for starfilenumber in ${starsAll[@]}
 do
 cd ./"stars-$starfilenumber"
 echo "running files in stars-$starfilenumber"
-	for filenumber in {1000..5000..100}
+if [ $starfilenumber -lt 250 ] 
+then
+	timeInSeconds=180s
+elif [ $starfilenumber -lt 600 ]
+then
+	timeInSeconds=480s 
+else
+	timeInSeconds=900s
+fi
+	for filenumber in {1..70..1}
 	do
 		echo ""
 		echo "running file $filenumber"
 	        cd "./run-$filenumber"
 		echo "BEGIN: [`date`]"
-	        timeout 1200s nbody6 <ini.dat> output
+	        timeout $timeInSeconds nbody6 <ini.dat> output
 	        if [ $? -eq 124 ]; then
 	                echo "WARNING: timeout reached"
 	        fi
