250d5e060c2111de16561cd4797d723eab225f15
Cesar Guerra <cesarg@wolfram.com>
2017-06-29 11:50:15 -0500

    addin new files

---
+++ nbody6/isolated/n250-N100/test1/create-ini.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "[`date`] Start"
+
+cd results/
+for randomseed in {1..100..1}
+do
+	mkdir "./run-$randomseed/"
+  	cat << EOF > ./run-$randomseed/ini.dat
+1 20.0
+250 1 10 $randomseed 60 1
+0.01 0.01 0.3 2.0 1.0 1500.0 1.0E-03 0.1 0.5
+0 0 1 0 1 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 2
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+4.0E-05 0.001 0.2 1.0 1.0E-06 0.001
+2.3 50.0 0.2 0 0 0.002 0 5.0
+0.5 0 0 0 0.25
+EOF
+	echo $randomseed
+done
+
+cd ..
+echo `pwd`
+echo "[`date`] End"
+echo "time elapsed: $SECONDS"
+echo all done
+
