650e348a3f9941454431e840a745fa0f530cd362
Cesar Guerra <cesarg@wolfram.com>
2017-07-07 09:52:11 -0500

    new sim files

--- nbody6/ext-field/point-mass/n250-N100/test1/create-ini.sh
+++ nbody6/ext-field/point-mass/n250-N100/test1/create-ini.sh
@@ -3,23 +3,24 @@
 echo "[`date`] Start"
 
 cd results/
-for randomseed in {1..100..1}
+for filenumber in {1..100..1}
 do
-	mkdir "./run-$randomseed/"
-  	cat << EOF > ./run-$randomseed/ini.dat
+	mkdir "./run-$filenumber/"
+  	cat << EOF > ./run-$filenumber/ini.dat
 1 20.0
-250 1 10 $randomseed 60 1
-0.01 0.01 0.3 2.0 1.0 1500.0 1.0E-03 0.1 0.5
+250 1 10 $RANDOM 60 1
+0.01 0.01 0.3 2.0 1.0 1500.0 1.0E-02 0.1 0.5
 0 0 1 0 1 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 2
+0 0 0 2 0 0 0 0 0 2
 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1
 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0
-4.0E-05 0.001 0.2 1.0 1.0E-06 0.001
+2.0E-05 0.0001 0.2 1.0 1.0E-06 0.001
 2.3 50.0 0.2 0 0 0.002 0 5.0
-0.5 0 0 0 0.25
+0.5 0 0 0 0.125
+4.D+6 8
 EOF
-	echo $randomseed
+	echo $filenumber
 done
 
 cd ..
