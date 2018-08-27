83913d5347469cc9229f9da64ea9f5e67802b443
Cesar Guerra <cesarg@wolfram.com>
2018-08-27 12:37:32 -0500

    new files for different number of stars

---
+++ nbody6/isolated/n1150/NSX-R01-parallel/nbody-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+jobwait() { while [ `jobs | wc -l` -gt $1 ]; do sleep .1; done }
+
+KERNELS=32
+
+echo "[`date`] Start"
+cd results/
+
+for filenumber in {1..250..1}
+do
+	echo ""
+	echo "running file $filenumber"
+	cd "./run-$filenumber"
+	time  (timeout 15m nbody6 <ini.dat> output) 2>> timings &
+	jobwait $KERNELS
+	cd ..
+done
+wait
+
+cd ..
+
+echo "[`date`] End"
+echo "elapsed time: $SECONDS"
+echo all done
---
+++ nbody6/isolated/n1300/NSX-R01-parallel/create-ini.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "[`date`] Start"
+
+cd results/
+for filenumber in {1..250..1}
+do
+	mkdir "./run-$filenumber/"
+  	cat << EOF > ./run-$filenumber/ini.dat
+1 20.0
+1000 1 10 $RANDOM 70 1
+0.01 0.01 0.3 1.0 1.0 7000.0 1.0E-03 1 0.5
+0 0 1 0 1 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 2
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+4.0E-05 0.001 0.2 1.0 1.0E-06 0.001
+2.3 50.0 0.2 0 0 0.002 0 5.0
+0.5 0 0 0 0.5
+EOF
+	echo $filenumber
+done
+
+cd ..
+echo `pwd`
+echo "[`date`] End"
+echo "time elapsed: $SECONDS"
+echo all done
+
