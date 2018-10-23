95eca7fca82785edd23f1697a2afd09a4674bfa7
cesarg <cesarg@wolfram.com>
2018-10-23 15:08:08 -0500

    improve scripts to deleteResults and create empty file to copy results to other machines

--- deleteResults.sh
+++ deleteResults.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+echo "delete results folders in current machine"
+
 find -name "results" -print | while read dir; do
-	echo "$dir"
-    rm -rf "$dir"/
+        echo "$dir"
 done
+
+read -p "press enter to continue ..."
+
+find -name "results" -print | while read dir; do
+	rm -rf "$dir"/
+done
+
 
 # c: cp by checksum
 # u: cp by file timing
