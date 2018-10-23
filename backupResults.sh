8e7edcc2c23569f455568b2fbd692efd8e01c019
cesarg <cesarg@wolfram.com>
2018-10-23 11:14:04 -0500

    improve bakup results script

--- backupResults.sh
+++ backupResults.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+echo "backup results folders to tars server"
+
 find -name "results" -print | while read dir; do
 	echo "$dir"
+done
+
+read -p "press enter to continue ..."
+
+find -name "results" -print | while read dir; do
     rsync -avuzP "$dir"/ cesarg@tars:/home/usr2/cesarg/git/workspace/"$dir"/
 done
+
 
 # c: cp by checksum
 # u: cp by file timing
