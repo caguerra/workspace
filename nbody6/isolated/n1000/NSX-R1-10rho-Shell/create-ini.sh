8591ad0702df20019aa7e73cbc3a16e9af1bb3ce
Cesar Guerra <caguerra@gmail.com>
2020-06-03 23:42:06 -0500

    new run files

---
+++ nbody6/isolated/n1000/NSX-R1-10rho-Shell/create-ini.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "[`date`] Start"
+
+mkdir ./results/
+
+cd results/
+for filenumber in {1..1000..1}
+do
+	mkdir "./run-$filenumber/"
+  	cat << EOF > ./run-$filenumber/ini.dat
+1 40.0
+1000 1 10 $RANDOM 80 1
+0.01 0.01 0.3 1.0 10.0 18000.0 1.0E-03 1 0.5
+0 0 1 0 1 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 6
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 1
+0 0 0 0 0 0 0 0 0 0
+2.0E-05 0.001 0.2 1.0 1.0E-06 0.001
+2.3 50.0 0.2 0 0 0.002 0 0
+0.5 0 0 0 1.0
+EOF
+
+done
+
+cd ..
+echo `pwd`
+echo "[`date`] End"
+echo "time elapsed: $SECONDS"
+echo all done
+
+
