71d5a45dd21ae4e2932111dc0d5be153bb80c3c8
cesarg <cesarg@wolfram.com>
2021-03-26 15:33:06 -0500

    additions to run-template

--- nbody6/isolated/scripts/FilterAndTest/run-template.m
+++ nbody6/isolated/scripts/FilterAndTest/run-template.m
@@ -1,8 +1,14 @@
-#!/usr/local/bin/MathematicaScript -script 
+#!/usr/bin/wolframscript -script
+
+(* 
+  - Without script option, graphics are not displayed
+  - In M11, this used to work:
+    #!/usr/local/bin/MathematicaScript -script 
+*)
 
 (*
-  * Script to run example test.nb
-  * To monitor evaluation of the notebook another Kernel should be used.
+  - Script to run example test.nb
+  - To monitor evaluation of the notebook another Kernel should be used.
 *)
 
 SetOptions[Developer`InstallFrontEnd, Developer`LaunchFlags -> "-display :1 -nogui -geometry 800x600+10+10 -default visual"];
